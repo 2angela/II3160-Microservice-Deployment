f6c79a298b351a62526403c82b09af0142f1f9de
angela <angelageraldine2@gmail.com>
2023-12-08 15:17:29 +0700

    cors

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import media, location, interactionLog, auth, user, menu_ingredients_composition, customization, order, deliverOrder
 import uvicorn
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
 
 app.include_router(auth.router)
 app.include_router(media.router)
